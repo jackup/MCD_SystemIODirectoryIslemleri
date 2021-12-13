dd177fba305b4d101e4bcb5deed4f899fbedf891
jackup <yk.58@hotmail.com>
2021-12-13 15:46:32 +0300

    Add project files.

---
+++ MCD_SystemIODirectoryIslemleri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_SystemIODirectoryIslemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YeniKlasorOlustur("c:\\Network Akademi");
+        }
+        static void YeniKlasorOlustur(string path)
+        {
+            DirectoryInfo DI = Directory.CreateDirectory(path);
+        }
+    }
+}
